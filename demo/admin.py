c825686b11c0189a877a2a68e21513bbbe0f0e61
laisd <shudonglai98@gmail.cm>
2022-04-08 01:07:37 -0400

    caculateCovidRisk函数完成，简简简易版决策树

--- demo/admin.py
+++ demo/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from demo import models
 
 # Register your models here.
--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import sqlite3
 
-# Create your views here.
+def dataLoad(sql):
+    cx = sqlite3.connect("../db.sqlite3")
+    cursor = cx.cursor()
+    cursor.execute(sql)
+    columns = [_[0].lower() for _ in cursor.description]
+    results = [dict(zip(columns, _)) for _ in cursor]
+    cursor.close()
+    return len(results)
+
+def calculateCovidRsik():
+    doc_Sql = """select * from demo_doctor_visit"""
+    doc = dataLoad(doc_Sql)
+    medicine_Sql = """select * from demo_medicine"""
+    medicine = dataLoad(medicine_Sql)
+    trip_Sql = """select * from demo_trip"""
+    trip = dataLoad(trip_Sql)
+    symptom_Sql = """select * from demo_symptom"""
+    symptom = dataLoad(symptom_Sql)
+
+    if (doc > 2 or trip > 2):
+        if (symptom > 2):
+            if (medicine > 2):
+                return True
+
+    return False
+
+print(calculateCovidRsik())
