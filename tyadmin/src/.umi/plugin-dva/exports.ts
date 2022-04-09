165a0d8a477a8642fd5abcd1201682cb20ae43af
zj19980122 <1796502603@qq.com>
2022-04-09 16:45:09 -0400

    Delete unnecessary Aurogenpages.

--- tyadmin/src/.umi/plugin-dva/exports.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-
-export { connect, useDispatch, useStore, useSelector } from 'D:/code/pycharm/csci_6234/tyadmin/node_modules/dva';
-export { getApp as getDvaApp } from './dva';
--- tyadmin/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'D:/code/pycharm/csci_6234/tyadmin/node_modules/react-helmet';
